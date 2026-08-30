06409774f133cfaa2c56d0838a4f3b42a9b86361
ginaintas-art <ginaintas@gmail.com>
2026-08-30 14:31:24 +1000

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
   const emitSourcemaps = mode === 'development'
 
   return {
-    base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : '/',
+    base: process.env.FIGMA_PUBLIC_URL ? `${process.env.FIGMA_PUBLIC_URL}/` : process.env.GITHUB_ACTIONS ? '/hobart-heritage-ar-prototype/' : '/',
     build: {
       sourcemap: emitSourcemaps ? 'inline' : false,
       minify: !emitSourcemaps,
